07c2a246ea0eda7c55fae54dca408845f1ae0f89
Бакир <106823128+bakir111@users.noreply.github.com>
2022-07-30 16:31:43 +0300

    new
    new

---
+++ urok_js_website/js/index.js
@@ -0,0 +1,24 @@
+let menu;
+let imgg;
+
+function the_icon_is_hidden(){
+  imgg=document.getElementById("img");
+  imgg.style.left="-100px";
+  menu=document.getElementById("menu");
+  menu.style.left="0px";
+}
+
+function aose(){
+  menu=document.getElementById("menu");
+  menu.style.left="-150px";
+  imgg=document.getElementById("img");
+  imgg.style.left="0px";
+  
+
+  console.log("clik");
+}
+
+
+
+
+
